973e650539376cd10c161513c4ddee08d197e3c2
Yelk11 <neighbour.matthew@gmail.com>
2025-11-13 21:06:03 -0500

    fixed camera system

--- src/util/screen.c
+++ src/util/screen.c
@@ -1,27 +1,54 @@
 #include "screen.h"
 #include "ncurses.h"
-
+#include "map.h"
 
 
 void update_screen(game_t* game)
 {
-    int my, mx;
-    getmaxyx(stdscr, my, mx);
-    for(int x = 0; x < mx; x++)
+    int scr_h, scr_w;
+    getmaxyx(stdscr, scr_h, scr_w);
+
+    int px = game->player->pos->x;
+    int py = game->player->pos->y;
+
+
+
+    // ---- CAMERA CALCULATION ----
+    int cam_x = px - scr_w / 2;
+    int cam_y = py - scr_h / 2;
+
+    // Clamp camera to map bounds
+    if (cam_x < 0) cam_x = 0;
+    if (cam_y < 0) cam_y = 0;
+    if (cam_x > MAP_WIDTH - scr_w) cam_x = MAP_WIDTH - scr_w;
+    if (cam_y > MAP_HEIGHT - scr_h) cam_y = MAP_HEIGHT - scr_h;
+
+    // ---- DRAW THE MAP WINDOW ----
+    for (int y = 0; y < scr_h; y++)
     {
-        for(int y = 0; y < my; y++)
+        for (int x = 0; x < scr_w; x++)
         {
-            if(game->map->map[y][x])
+            int map_x = cam_x + x;
+            int map_y = cam_y + y;
+
+            char ch = 'X';   // default tile
+            if (map_y >= 0 && map_y < MAP_HEIGHT &&
+                map_x >= 0 && map_x < MAP_WIDTH)
             {
-                mvaddch(y,x,game->map->map[y][x]);
-            }else{
-                mvaddch(y,x,'X');
+                ch = game->map->map[map_y][map_x];
+                if (!ch) ch = 'X';
             }
+
+            mvaddch(y, x, ch);
         }
     }
-    mvaddch(game->player->pos->y, game->player->pos->x, game->player->ascii_char);
+
+    // ---- DRAW PLAYER WITH CAMERA OFFSET ----
+    mvaddch(py - cam_y, px - cam_x, game->player->ascii_char);
+
     refresh();
 }
+
 
 
 
